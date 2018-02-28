3d1e7a8a9c0c34eab11112cfd54608775da0cdeb
varsha yadav <yadavvarsha70@gmail.com>
2018-02-28 19:17:16 +0530

    my project setup complete succesfully.

---
+++ Exampleshopback/src/main/java/com/shop/Exampleshopback/App.java
@@ -0,0 +1,13 @@
+package com.shop.Exampleshopback;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
